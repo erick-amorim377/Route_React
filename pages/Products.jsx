650780d109b93787c7c41df42b830956015174a1
erick-amorim377 <erickamorim221@gmail.com>
2026-03-16 11:02:23 -0300

    Carregamento dinâmico de dados

--- pages/Products.jsx
+++ pages/Products.jsx
@@ -1,10 +1,24 @@
 import { useParams } from "react-router-dom";
+import { useFetch } from "../hook/useFetch";
+
 
 const Products = () => {
     const { id } = useParams();
+
+    const url = 'http://localhost:3001/products/' + id;
+    const {data: product, loading, error} = useFetch(url);
+
   return (
     <>
         <p>ID do produto: {id}</p>
+        {error && <p>Ocorreu um error...</p>}
+        {loading && <p>Carregando...</p>}
+        {product && (
+            <div>
+                <h1>{product.name}</h1>
+                <p>R${product.price}</p>
+            </div>
+        )}
     </>
   )
 }
